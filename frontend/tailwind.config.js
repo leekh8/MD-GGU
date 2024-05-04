9cc9a57646a2357a20dff770ee975285fe4ac955
leekh8 <amysun125@gmail.com>
2024-05-04 11:03:55 +0900

    Chore: upload package.json, tailwind.config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-blue": "#005f73",
+        "brand-yellow": "#ffc300",
+        "brand-red": "#dc2f02",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
