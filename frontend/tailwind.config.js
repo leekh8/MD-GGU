101628b9eda5f1e7b0e4c55904c392846d266db4
leekh8 <amysun125@gmail.com>
2024-05-08 19:34:18 +0900

    feat: change brand color and style setting

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,13 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "brand-blue": "#005f73",
-        "brand-yellow": "#ffc300",
-        "brand-red": "#dc2f02",
+        "brand-blue": "#003d5b",
+        "brand-yellow": "#ffa600",
+        "brand-red": "#d62828",
+        "brand-grey": "#A9A9A9",
       },
       fontFamily: {
-        sans: ["Inter", "sans-serif"],
+        sans: ["Pretendard", "Inter", "sans-serif"],
         serif: ["Merriweather", "serif"],
+      },
+      boxShadow: {
+        strong:
+          "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+        soft: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
       },
     },
   },
