4bbfb59bb2977feb2d6aeed7c54c8364af3bf9f7
leekh8 <amysun125@gmail.com>
2024-05-09 19:30:55 +0900

    feat: add login page

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { useAuth } from "../components/AuthProvider";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(""); // 에러 메시지 상태
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!username || !password) {
+      setError("All fields are required");
+      return;
+    }
+    try {
+      auth.login(username, password);
+      navigate("/"); // Redirect to home after login
+    } catch (e) {
+      setError("Login failed: " + e.message); // 예외 처리와 에러 메시지 설정
+    }
+  };
+
+  return (
+    <div className="max-w-lg mx-auto mt-10 px-4 py-8 shadow-lg rounded-lg">
+      <h1 className="text-xl font-semibold text-center text-brand-blue">
+        Login to Your Account
+      </h1>
+      {error && <p className="text-red-500 text-center">{error}</p>}{" "}
+      {/* 에러 메시지 */}
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-brand-blue focus:border-brand-blue sm:text-sm"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-brand-blue focus:border-brand-blue sm:text-sm"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-brand-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
