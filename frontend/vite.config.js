172cca596da6c9b1bf282dc81305364fe6097e6e
leekh8 <amysun125@gmail.com>
2024-05-17 19:08:06 +0900

    fix: delete babel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { babel } from "@rollup/plugin-babel";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({
-      babelHelpers: "runtime",
-      plugins: ["@babel/plugin-transform-runtime"],
-    }),
-  ],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
