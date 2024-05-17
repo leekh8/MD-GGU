e46dddefa75c3983015938c8e14bf4b460e1f6b4
leekh8 <amysun125@gmail.com>
2024-05-17 18:42:17 +0900

    chore: add babel config with vitest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { babel } from "@rollup/plugin-babel";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-react"],
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
+    transformMode: {
+      web: [/.[jt]sx?/],
+    },
   },
 });
