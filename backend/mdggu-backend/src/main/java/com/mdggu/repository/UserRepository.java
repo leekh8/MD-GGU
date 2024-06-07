fdaf35db0f26b6aff410a90671d14c93e7c7798d
leekh8 <amysun125@gmail.com>
2024-06-07 20:52:27 +0900

    feat: edit user repository for user features

--- backend/mdggu-backend/src/main/java/com/mdggu/repository/UserRepository.java
+++ backend/mdggu-backend/src/main/java/com/mdggu/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.mdggu.repository;public class UserRepository {
+// User 엔티티에 대한 데이터 액세스
+package com.mdggu.repository;
+
+import com.mdggu.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
