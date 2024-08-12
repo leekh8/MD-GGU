4b97766eb98e8e76c8e46a031629642d9e39411e
leekh8 <amysun125@gmail.com>
2024-08-12 09:45:35 +0900

    fix: loadPath setting error correction

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -13,7 +13,7 @@ i18n
       escapeValue: false, // React에서 XSS 공격 방지를 위해 escape 처리를 하므로, 여기서는 비활성화
     },
     backend: {
-      loadPath: "/locales/{{ns}}.json",
+      loadPath: "/locales/{{lng}}.json",
     },
   });
 
