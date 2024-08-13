fd5576fec1e7cc6453a7405f8e1771e720043d49
leekh8 <amysun125@gmail.com>
2024-08-13 13:15:51 +0900

    fix: Modify translation file path

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,13 +1,20 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
 
-i18n.use(initReactI18next).init({
-  lng: "ko", // 초기 언어 설정 (한국어)
-  fallbackLng: "en", // 지원되지 않는 언어일 경우 fallback 언어 (영어)
-  debug: true, // 개발 모드에서는 디버깅 활성화
-  interpolation: {
-    escapeValue: false, // React에서 XSS 공격 방지를 위해 escape 처리를 하므로, 여기서는 비활성화
-  },
-});
+i18n
+  .use(Backend) // 번역 파일을 가져오는 도구 사용
+  .use(initReactI18next) // React와 i18next 연결
+  .init({
+    lng: "ko", // 초기 언어 설정 (한국어)
+    fallbackLng: "en", // 지원되지 않는 언어일 경우 fallback 언어 (영어)
+    debug: true, // 개발 모드에서는 디버깅 활성화
+    interpolation: {
+      escapeValue: false, // React에서 XSS 공격 방지를 위해 escape 처리를 하므로, 여기서는 비활성화
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/translation.json",
+    },
+  });
 
 export default i18n;
