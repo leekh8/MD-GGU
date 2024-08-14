8b19fced1f21ec994d5c513063bdfd1927885aa7
leekh8 <amysun125@gmail.com>
2024-08-14 10:57:22 +0900

    fix: Debug Mode Off

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -8,7 +8,7 @@ i18n
   .init({
     lng: "ko", // 초기 언어 설정 (한국어)
     fallbackLng: "en", // 지원되지 않는 언어일 경우 fallback 언어 (영어)
-    debug: true, // 개발 모드에서는 디버깅 활성화
+    debug: false, // 개발 모드에서는 디버깅 활성화
     interpolation: {
       escapeValue: false, // React에서 XSS 공격 방지를 위해 escape 처리를 하므로, 여기서는 비활성화
     },
