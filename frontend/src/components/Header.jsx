b06a80d767769d85b938b7cd9fe9ff8932562f83
leekh8 <amysun125@gmail.com>
2024-08-17 16:31:54 +0900

    design: Add hamburger menu icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <Link to="/" className="text-lg md:text-2xl font-bold font-sans">
           {t("mdggu")}
         </Link>
-        <nav className="items-center">
+        <nav className="hidden md:flex items-center">
           <ul className="flex space-x-4">
             <li>
               <Link
@@ -80,6 +80,22 @@ const Header = () => {
             </li>
           </ul>
         </nav>
+        <button className="md:hidden text-white focus:outline-none">
+          {/* 햄버거 메뉴 아이콘 */}
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
       </div>
     </header>
   );
