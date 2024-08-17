9bd82f210f728aae61ebfb9fe4d972270d83b286
leekh8 <amysun125@gmail.com>
2024-08-17 12:12:53 +0900

    rename: Integrate css files and settings

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,6 @@
 // ReactDOM을 통해 앱 마운트
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles.css";
