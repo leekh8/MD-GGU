72c47c65081cf17f7e73609dbb65b2f0f8df1a27
leekh8 <amysun125@gmail.com>
2024-08-18 09:26:22 +0900

    design: Change icon color

--- frontend/src/components/LanguageSwitcher.jsx
+++ frontend/src/components/LanguageSwitcher.jsx
@@ -14,43 +14,41 @@ const LanguageSwitcher = () => {
   };
 
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as="div" className="relative inline-block text-left ">
       <div>
-        <Menu.Button className="flex items-center text-brand-yellow hover:text-brand-blue py-1 px-2 rounded-md transition-colors duration-200">
+        <Menu.Button className="flex items-center py-0.5 px-1 rounded-md">
           <GlobeAltIcon className="w-5 h-5 mr-1" aria-hidden="true" />
-          <ChevronDownIcon className="w-4 h-4 ml-2" aria-hidden="true" />
+          <ChevronDownIcon className="w-4 h-4 ml-1" aria-hidden="true" />
         </Menu.Button>
       </div>
 
-      <Menu.Items className="absolute right-0 w-max mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <div className="py-1">
-          <Menu.Item>
-            {({ active }) => (
-              <button
-                type="button"
-                onClick={() => changeLanguage("ko")}
-                className={`${
-                  active ? "bg-brand-gray-light text-gray-900" : "text-gray-700"
-                } group flex rounded-md items-center w-full px-4 py-2 text-sm`}
-              >
-                한국어
-              </button>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <button
-                type="button"
-                onClick={() => changeLanguage("en")}
-                className={`${
-                  active ? "bg-brand-gray-light text-gray-900" : "text-gray-700"
-                } group flex rounded-md items-center w-full px-4 py-2 text-sm`}
-              >
-                English
-              </button>
-            )}
-          </Menu.Item>
-        </div>
+      <Menu.Items className="absolute right-0 mt-2 py-2 w-36 bg-white text-black rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Item>
+          {({ active }) => (
+            <button
+              type="button"
+              onClick={() => changeLanguage("ko")}
+              className={`${
+                active ? "bg-brand-gray-light " : "dropdown-item"
+              } block w-full text-left px-4 py-2 text-sm`}
+            >
+              한국어
+            </button>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          {({ active }) => (
+            <button
+              type="button"
+              onClick={() => changeLanguage("en")}
+              className={`${
+                active ? "bg-brand-gray-light " : "dropdown-item"
+              } block w-full text-left px-4 py-2 text-sm`}
+            >
+              English
+            </button>
+          )}
+        </Menu.Item>
       </Menu.Items>
     </Menu>
   );
