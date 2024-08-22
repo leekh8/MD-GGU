2417bfa0498d35b83f891f0bb441fa1e347233c3
leekh8 <amysun125@gmail.com>
2024-08-22 20:19:53 +0900

    fix: Remove username field

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -6,7 +6,6 @@ import { Helmet } from "react-helmet";
 
 const SignupPage = () => {
   const { t } = useTranslation();
-  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -15,12 +14,12 @@ const SignupPage = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!username || !email || !password) {
+    if (!email || !password) {
       setError("All fields are required");
       return;
     }
     try {
-      register(username, email, password);
+      register(email, password);
       navigate("/"); // 회원가입 후 홈페이지로 리디렉션
     } catch (e) {
       setError("Signup failed: " + e.message);
@@ -39,22 +38,6 @@ const SignupPage = () => {
       </h1>
       {error && <p className="text-red-500 text-center">{error}</p>}
       <form onSubmit={handleSubmit} className="space-y-4">
-        {/* <div>
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-brand-blue focus:border-brand-blue sm:text-sm"
-          />
-        </div> */}
         <div>
           <label
             htmlFor="email"
