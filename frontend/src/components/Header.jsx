0f6582c99a5e7ad7da16d65aa408a8d2e5aefd95
leekh8 <amysun125@gmail.com>
2024-08-29 15:11:58 +0900

    feat: Add ARIA Properties to Header Components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -60,7 +60,11 @@ const Header = () => {
   return (
     <header className="bg-brand-blue text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-lg md:text-2xl font-bold font-sans">
+        <Link
+          to="/"
+          className="text-lg md:text-2xl font-bold font-sans"
+          aria-label="홈페이지로 이동"
+        >
           {t("mdggu")}
         </Link>
         <nav className="hidden md:flex items-center space-x-4">
@@ -68,6 +72,7 @@ const Header = () => {
             <li>
               <Link
                 to="/"
+                aria-label="홈페이지로 이동"
                 className="hover:text-brand-yellow transition-colors duration-300"
               >
                 {t("home")}
@@ -77,6 +82,7 @@ const Header = () => {
             <li>
               <Link
                 to="/editor"
+                aria-label="편집기로 이동"
                 className="hover:text-brand-yellow transition-colors duration-300"
               >
                 {t("editor")}
@@ -89,6 +95,8 @@ const Header = () => {
         </nav>
         <div className="md:hidden relative">
           <button
+            aria-label="메뉴 열기"
+            aria-expanded={menuOpen}
             className="text-white focus:outline-none"
             onClick={() => setMenuOpen(!menuOpen)}
           >
@@ -111,6 +119,7 @@ const Header = () => {
               <li className="block px-4 py-2 text-sm hover:bg-gray-200">
                 <Link
                   to="/"
+                  aria-label="홈페이지로 이동"
                   onClick={() => setMenuOpen(false)}
                   className="block w-full h-full"
                 >
@@ -121,6 +130,7 @@ const Header = () => {
               <li className="block px-4 py-2 text-sm hover:bg-gray-200">
                 <Link
                   to="/editor"
+                  aria-label="편집기로 이동"
                   onClick={() => setMenuOpen(false)}
                   className="block w-full h-full"
                 >
