ce4d08a956e3e409df92e71b0e61cb4a41cc94fb
leekh8 <amysun125@gmail.com>
2024-08-30 11:42:11 +0900

    fix: Modify regular pattern from CustMatcher
    Signed-off-by: leekh8 <amysun125@gmail.com>

--- backend/mdggu-backend/src/main/java/com/mdggu/config/CustomRequestMatcher.java
+++ backend/mdggu-backend/src/main/java/com/mdggu/config/CustomRequestMatcher.java
@@ -17,9 +17,11 @@ public class CustomRequestMatcher implements RequestMatcher {
     }
 
     private String normalizePattern(String pattern) {
+        // 패턴이 슬래시로 끝나면 슬래시 제거
         if (pattern.endsWith("/")) {
             pattern = pattern.substring(0, pattern.length() - 1);
         }
-        return pattern;
+        // API 엔드포인트에 대해 정규식 패턴을 생성
+        return pattern.replace("*", ".*");
     }
 }
