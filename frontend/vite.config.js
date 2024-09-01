28725b99d3086eadb5f12b32e235cc05526b9927
leekh8 <amysun125@gmail.com>
2024-09-02 08:34:52 +0900

    fix: Modifying the Front-End Start Path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import path from "path";
 const port = parseInt(process.env.VITE_PORT) || 3000;
 
 export default defineConfig({
-  root: path.resolve(__dirname, "src"), // 프로젝트의 루트를 src 폴더로 설정
   publicDir: path.resolve(__dirname, "public"), // public 폴더 경로를 명시적으로 지정
   plugins: [react()],
   server: {
