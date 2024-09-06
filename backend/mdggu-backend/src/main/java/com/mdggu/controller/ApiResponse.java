ff98f634b0ff655bb1520708c85260ea767cfd05
leekh8 <amysun125@gmail.com>
2024-09-06 15:17:15 +0900

    fix: Add generic type to ApiResponse

--- backend/mdggu-backend/src/main/java/com/mdggu/controller/ApiResponse.java
+++ backend/mdggu-backend/src/main/java/com/mdggu/controller/ApiResponse.java
@@ -1,12 +1,19 @@
 package com.mdggu.controller;
 
-public class ApiResponse {
+public class ApiResponse<T> {
     private boolean success;
     private String message;
+    private T data;
 
     public ApiResponse(boolean success, String message) {
         this.success = success;
         this.message = message;
+    }
+
+    public ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
     }
 
     public boolean isSuccess() {
@@ -23,5 +30,9 @@ public class ApiResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public void setData(T data) {
+        this.data = data;
     }
 }
