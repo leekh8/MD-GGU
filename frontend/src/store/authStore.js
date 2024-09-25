1c98c8609d9a6b100aafadfbb0f338a9d7c6b5be
leekh8 <amysun125@gmail.com>
2024-09-25 19:00:43 +0900

    feat: Use Zustand to define isAuthenticated, role states and functions that change them

---
+++ frontend/src/store/authStore.js
@@ -0,0 +1,10 @@
+import create from "zustand";
+
+const useAuthStore = create((set) => ({
+  isAuthenticated: false,
+  role: "GUEST", // GUEST, USER, ADMIN
+  setIsAuthenticated: (value) => set({ isAuthenticated: value }),
+  setRole: (value) => set({ role: value }),
+}));
+
+export default useAuthStore;
