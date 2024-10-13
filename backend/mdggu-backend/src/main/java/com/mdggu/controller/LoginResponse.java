96f2e4a622b67d376b86a204058b62beacddfc68
leekh8 <amysun125@gmail.com>
2024-10-13 13:48:21 +0900

    fix: Added refresh token field to LoginResponse class

--- backend/mdggu-backend/src/main/java/com/mdggu/controller/LoginResponse.java
+++ backend/mdggu-backend/src/main/java/com/mdggu/controller/LoginResponse.java
@@ -2,16 +2,22 @@ package com.mdggu.controller;
 
 public class LoginResponse {
     private String accessToken;
+    private String refreshToken;
     private String csrfToken;
 
-    public LoginResponse(String accessToken, String csrfToken) {
+    public LoginResponse(String accessToken, String refreshToken, String csrfToken) {
         this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
         this.csrfToken = csrfToken;
     }
 
     // getter 메서드
     public String getAccessToken() {
         return accessToken;
+    }
+
+    public String getrefreshToken() {
+        return refreshToken;
     }
 
     public String getCsrfToken() {
