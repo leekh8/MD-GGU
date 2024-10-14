d7b225d5746e897bc740c04c15a4179c6d60adea
leekh8 <amysun125@gmail.com>
2024-10-14 13:58:42 +0900

    fix: Check if the Request Interceptor has expired and if it has expired, call the refreshAccessToken function to obtain a new AccessToken

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,6 +10,11 @@ const BACKEND_URL =
 const apiClient = axios.create({
   baseURL: BACKEND_URL,
   withCredentials: true, // 쿠키를 포함하여 요청 보내기
+  xsrfCookieName: "jwtToken", // 서버에서 설정한 쿠키 이름과 일치해야 함
+  xsrfHeaderName: "X-XSRF-TOKEN", // 기본값 사용
+  headers: {
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+  },
 });
 
 // 엔드포인트 상수 정의
@@ -21,36 +26,27 @@ const ENDPOINTS = {
   DOCUMENTS: "/api/v1/documents",
   DOCUMENT_BY_ID: "/api/v1/documents/", // 동적 ID 추가 필요
   STATUS: "/status",
+  REFRESH: "/refresh",
 };
 
-// 쿠키에서 JWT 토큰 가져오는 함수
-function getJwtTokenFromCookie() {
-  const cookies = document.cookie.split(";");
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    if (cookie.startsWith("jwtToken=")) {
-      return cookie.substring("jwtToken=".length, cookie.length);
-    }
-  }
-  return null;
-}
-
-// apiClient 인스턴스에 인터셉터 추가
+// API 요청 시 Access Token 만료 여부 확인 및 갱신
 apiClient.interceptors.request.use(
-  (config) => {
-    // 쿠키에서 JWT 토큰 가져오기
-    const jwtToken = getJwtTokenFromCookie();
-    if (jwtToken) {
-      const decodedToken = jwtDecode(jwtToken); // jwt-decode 라이브러리 사용
-      if (decodedToken.exp < Date.now() / 1000) {
-        // 토큰 만료
-        localStorage.removeItem("token"); // 토큰 제거
-        window.location.href = "/login"; // 로그인 페이지로 리다이렉트
-        return Promise.reject(new Error("Token expired"));
+  async (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      try {
+        const decodedToken = jwtDecode(accessToken);
+        if (decodedToken.exp < Date.now() / 1000) {
+          // Access Token 만료됨
+          const newAccessToken = await refreshAccessToken(); // 새로운 Access Token 발급
+          config.headers.Authorization = `Bearer ${newAccessToken}`; // 헤더 업데이트
+          const newCsrfToken = localStorage.getItem("csrfToken"); // 갱신된 CSRF Token 가져오기
+          config.headers["X-CSRF-TOKEN"] = newCsrfToken; // CSRF Token 헤더 업데이트
+        }
+      } catch (error) {
+        console.error("Error refreshing access token:", error);
       }
-      config.headers.Authorization = `Bearer ${jwtToken}`;
     }
-
     return config;
   },
   (error) => {
@@ -58,13 +54,20 @@ apiClient.interceptors.request.use(
   }
 );
 
+// 쿠키에서 JWT 토큰 가져오는 함수
+export function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
 // 사용자 인증 관련 함수들
 export function register(email, password) {
   return apiClient
     .post(ENDPOINTS.REGISTER, { email, password })
     .then((response) => response.data)
     .catch((error) => {
-      console.error("register error:", error.response || error); // 에러 메시지 출력
+      console.error("API - Error in register:", error.response || error); // 에러 메시지 출력
       throw error.response?.data || error;
     });
 }
@@ -75,10 +78,37 @@ export function login(email, password) {
       email,
       password,
     })
-    .then((response) => response.data)
+    .then((response) => {
+      const { data } = response; // 응답 데이터 추출
+      const { accessToken } = data; // Access Token 추출
+
+      // 응답 헤더에서 refreshToken과 csrfToken 추출
+      const refreshToken = getCookie("refreshToken");
+      const csrfToken = response.headers["x-csrf-token"];
+
+      // Access Token, Refresh Token, CSRF Token 확인
+      console.log("API - accessToken in login #1:", accessToken);
+      console.log("API - refreshToken in login #1:", refreshToken);
+      console.log("API - csrfToken in login #1:", csrfToken);
+
+      // 응답 데이터에 refreshToken과 csrfToken 추가
+      data.refreshToken = refreshToken;
+      data.csrfToken = csrfToken;
+
+      console.log("API - accessToken in login #3:", accessToken);
+      console.log("API - refreshToken in login #3:", refreshToken);
+      console.log("API - csrfToken in login #3:", csrfToken);
+
+      return data; // 수정된 응답 데이터 반환
+    })
     .catch((error) => {
-      console.error("login error:", error.response || error); // 에러 메시지 출력
-      throw error.response?.data || error;
+      console.error("API - Error in login:", error.response || error); // 에러 메시지 출력
+      if (error.response && error.response.data) {
+        // error.response가 있고 data 속성이 있는지 확인
+        throw error.response.data;
+      } else {
+        throw error; // error.response가 없으면 원래 에러 throw
+      }
     });
 }
 
@@ -87,25 +117,61 @@ export function logout() {
     .post(ENDPOINTS.LOGOUT)
     .then((response) => response.data)
     .catch((error) => {
-      console.error("logout error:", error.response || error); // 에러 메시지 출력
+      console.error("API - Error in logout:", error.response || error); // 에러 메시지 출력
       throw error.response?.data || error;
     });
 }
 
-export function getUser() {
-  return apiClient
-    .get(ENDPOINTS.ME)
-    .then((response) => {
-      if (response.success) {
-        return response.data; // 사용자 정보 반환
-      } else {
-        throw new Error(response.data.message); // 에러 발생 시 에러 메시지와 함께 예외 throw
-      }
-    })
-    .catch((error) => {
-      console.error("getUser error:", error.response || error); // 에러 메시지 출력
-      throw error.response?.data || error;
-    });
+export function getUser(config = {}) {
+  const accessToken = localStorage.getItem("accessToken"); // localStorage에서 Access Token 가져오기
+  const csrfToken = localStorage.getItem("csrfToken"); // localStorage에서 CSRF Token 가져오기
+
+  if (accessToken) {
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${accessToken}`,
+      "X-CSRF-TOKEN": csrfToken, // CSRF Token 헤더에 추가
+    };
+
+    return apiClient
+      .get(ENDPOINTS.ME, config)
+      .then((response) => {
+        if (response.success) {
+          return response.data; // 사용자 정보 반환
+        } else {
+          throw new Error(response.message); // 에러 발생 시 에러 메시지와 함께 예외 throw
+        }
+      })
+      .catch((error) => {
+        console.error("API - Error in getUser:", error.response || error); // 에러 메시지 출력
+        throw error.response?.data || error;
+      });
+  }
+}
+
+// Access Token 갱신 함수
+const refreshAccessToken = async () => {
+  try {
+    const response = await apiRefreshToken(); // Refresh Token을 사용하여 Access Token 갱신 API 호출
+    if (response.success) {
+      const { accessToken, csrfToken } = response.data; // response.data에서 csrfToken 추출
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("csrfToken", csrfToken); // csrfToken 값만 저장
+      return accessToken;
+    } else {
+      // Refresh Token이 유효하지 않은 경우 로그아웃 처리
+      logout();
+      throw new Error("Refresh token invalid");
+    }
+  } catch (error) {
+    console.error("Failed to refresh access token:", error);
+    throw error;
+  }
+};
+
+export function apiRefreshToken() {
+  const refreshToken = localStorage.getItem("refreshToken"); // localStorage에서 Refresh Token 가져오기
+  return apiClient.post(ENDPOINTS.REFRESH, { refreshToken }); // Refresh Token을 요청 본문에 포함
 }
 
 // 문서 관련 함수들
