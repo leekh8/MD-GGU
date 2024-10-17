b486ae56530fadaeefe0bfed4c665644251e7543
leekh8 <amysun125@gmail.com>
2024-10-17 16:44:59 +0900

    feat: Create a MyPage.jsx component

---
+++ frontend/src/pages/MyPage.jsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../components/AuthProvider";
+import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
+
+const MyPage = () => {
+  const { t } = useTranslation();
+  const { user, updateUser } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      setEmail(user.email);
+    }
+  }, [user]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError("");
+
+    if (password !== confirmPassword) {
+      setError(t("passwordsDoNotMatch"));
+      return;
+    }
+    // TODO: 현재 비밀번호 확인 로직 추가
+    try {
+      // TODO: 비밀번호 암호화 로직 추가
+      const updatedUser = { ...user, email, password };
+      await updateUser(updatedUser);
+      // 성공적으로 업데이트 되면 메시지 표시 또는 페이지 이동
+    } catch (error) {
+      setError(error.message || t("failedToUpdateProfile"));
+    }
+  };
+
+  return (
+    <div className="max-w-lg mx-auto mt-10 px-4 py-8 shadow-lg rounded-lg">
+      <Helmet>
+        <title>
+          {t("mdggu")} ・ {t("myPage")}
+        </title>
+      </Helmet>
+      <h1 className="text-xl font-semibold text-center text-brand-blue">
+        {t("myPage")}
+      </h1>
+      {error && <p className="text-red-500 text-center">{error}</p>}
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            {t("email")}
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder={t("email")}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-smfocus:outline-none focus:ring-brand-blue focus:border-brand-blue sm:text-sm"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            {t("newPassword")}
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder={t("newPassword")}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm  
+focus:outline-none focus:ring-brand-blue  
+focus:border-brand-blue sm:text-sm"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="confirmPassword"
+            className="block text-sm font-medium text-gray-700"
+          >
+            {t("confirmPassword")}
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder={t("confirmPassword")}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-brand-blue  
+focus:border-brand-blue sm:text-sm"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-brand-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow"
+        >
+          {t("updateProfile")}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default MyPage;
